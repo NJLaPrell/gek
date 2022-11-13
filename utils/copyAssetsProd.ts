6afb971598de6f2124072f3a5da460259dcfb1eb
NJLAPRELL <nick@laprell.org>
2022-11-12 17:10:23 -0700

    split package.json

--- utils/copyAssetsProd.ts
+++ utils/copyAssetsProd.ts
@@ -3,3 +3,5 @@ const shell = require('shelljs');
 shell.cp( '-R', 'server/lib', 'ytlist-docker/server/dist' );
 shell.cp( '-R', 'server/models', 'ytlist-docker/server/dist' );
 shell.cp( '-R', 'server/routes', 'ytlist-docker/server/dist' );
+shell.cp( '-R', 'server/package.json', 'ytlist-docker/server' );
+shell.cp( '-R', 'server/package-lock.json', 'ytlist-docker/server' );
