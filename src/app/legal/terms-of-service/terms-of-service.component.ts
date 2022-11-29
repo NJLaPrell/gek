85b3b006f7cca5b426c547ead6996d944019d5b1
NJLAPRELL <nick@laprell.org>
2022-11-29 10:31:52 -0700

    #22 - Fixed TOS HTML and included link to YouTube TOS.

--- src/app/legal/terms-of-service/terms-of-service.component.ts
+++ src/app/legal/terms-of-service/terms-of-service.component.ts
@@ -4,7 +4,8 @@ import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
   selector: 'app-terms-of-service',
   templateUrl: './terms-of-service.component.html',
   schemas: [ NO_ERRORS_SCHEMA ],
-  standalone: true
+  standalone: true,
+  styleUrls: ['./terms-of-service.component.scss']
 })
 export class TermsOfServiceComponent {
 
