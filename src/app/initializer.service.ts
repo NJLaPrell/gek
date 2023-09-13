69b36345103f6ec8f7a85a735440e4bd6dbb1f9a
NJLAPRELL <nick@laprell.org>
2023-09-13 16:55:04 -0700

    #43 - Fixing sweep mistakes.

--- src/app/initializer.service.ts
+++ src/app/initializer.service.ts
@@ -1,53 +1,68 @@
 import { Injectable, Injector, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { concat, Observable, of, interval, merge, Subscription, switchMap } from 'rxjs';
+import {
+  concat,
+  Observable,
+  of,
+  interval,
+  merge,
+  Subscription,
+  switchMap,
+} from 'rxjs';
 import { AppState } from './state';
 import { getAuthState } from './state/actions/auth.actions';
 import { getHistory } from './state/actions/history.actions';
-import { getSubscriptions, getUncachedLists } from './state/actions/list.actions';
+import {
+  getSubscriptions,
+  getUncachedLists,
+} from './state/actions/list.actions';
 import { getPreferences } from './state/actions/preferences.actions';
 import { getRules } from './state/actions/rules.actions';
 import { selectAuthenticated } from './state/selectors/auth.selectors';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InitializerService implements OnDestroy {
-export class InitializerService {
-  constructor(
-        private injector: Injector,
-        private store: Store<AppState>
-  ) { 
-    this.store.select(selectAuthenticated).subscribe(authenticated => this.initApp(authenticated));
-    this.historyUpdateSubscription = new Subscription();
+  constructor(private injector: Injector, private store: Store<AppState>) {
+    this.store
+      .select(selectAuthenticated)
+      .subscribe((authenticated) =>
+        typeof authenticated === 'undefined'
+          ? null
+          : this.initApp(authenticated)
+      );
   }
 
-  private historyUpdateSubscription: Subscription;
-  
+  private historyUpdateSubscription: Subscription = new Subscription();
+
+  ngOnDestroy(): void {
+    this.historyUpdateSubscription.unsubscribe();
+  }
+
   private initApp(authenticated: boolean) {
     if (authenticated) {
       this.historyUpdateSubscription = merge(
         this.getLists(),
+        this.getHistory(),
         interval(60000).pipe(switchMap(() => this.getHistory())),
         this.getRules(),
         this.getSubscriptions(),
         this.getPreferences()
       ).subscribe();
     }
-  ```
-    constructor(
-          private injector: Injector,
-          private store: Store<AppState>
-    ) { 
-      this.store.select(selectAuthenticated).subscribe(authenticated => this.initApp(authenticated));
-    }
-  
-    private historyUpdateSubscription: Subscription = new Subscription();
-  ```
+  }
+
+  private getLists(): Observable<any> {
+    this.store.dispatch(getUncachedLists());
+    return of([]);
+  }
+
   private getHistory(): Observable<any> {
     this.store.dispatch(getHistory());
     return of([]);
   }
+
   private getRules(): Observable<any> {
     this.store.dispatch(getRules());
     return of([]);
@@ -67,8 +82,6 @@ export class InitializerService {
   }
 
   init(): Promise<any> {
-    return concat(
-      this.getAuthenticated()
-    ).toPromise();
+    return concat(this.getAuthenticated()).toPromise();
   }
 }
